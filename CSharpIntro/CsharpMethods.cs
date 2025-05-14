bd0408647d7cb435d6222233fcc5f65ca7af2050
ManeeshChintalapudi12 <chintalapudimaneesh@gmail.com>
2025-05-14 16:50:33 -0500

    Deleted bin and onj

--- CSharpIntro/CsharpMethods.cs
+++ CSharpIntro/CsharpMethods.cs
@@ -1,16 +1,16 @@
 // //Methods // 
 
-// class CsharpMethods
-// {
-//     public static void MyMethod()
-//     {
-//         PrintPerson("Liam", 5);
-//         PrintPerson("Jenny", 8);
-//         PrintPerson("Anja", 31);
-//     }
+class CsharpMethods
+{
+    public static void MyMethod()
+    {
+        PrintPerson("Liam", 5);
+        PrintPerson("Jenny", 8);
+        PrintPerson("Anja", 31);
+    }
 
-//     private static void PrintPerson(string fname, int age)
-//     {
-//         Console.WriteLine(fname + " is " + age);
-//     }
-// }
+    private static void PrintPerson(string fname, int age)
+    {
+        Console.WriteLine(fname + " is " + age);
+    }
+}
