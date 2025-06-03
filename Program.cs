e08287c7923bb9636a76560889bcd19f85d0985a
ManeeshChintalapudi12 <chintalapudimaneesh@gmail.com>
2025-06-03 18:57:32 -0500

    Displayed Variables for Char, String, Doble, and Boolean

--- Program.cs
+++ Program.cs
@@ -3,7 +3,20 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
         Variables variables = new Variables();
         variables.Integer();
+
+        Variables obj = new Variables();
+        obj.Double();
+
+        Variables name = new Variables();
+        name.Char();
+
+        Variables myName = new Variables();
+        myName.String();
+
+        Variables mybool = new Variables();
+        mybool.Boolean();
     }
 }
--- Variables/Variables.cs
+++ Variables/Variables.cs
@@ -5,4 +5,30 @@ public class Variables
         int i = 10;
         Console.WriteLine(i);
     }
+
+    public void Double()
+    {
+        double j = 10;
+        Console.WriteLine(j);
+    }
+
+
+    public void Char()
+    {
+        char j = 'a';
+        Console.WriteLine(j);
+    }
+
+    public void String()
+    {
+        string j = "Mani";
+        Console.WriteLine(j);
+    }
+
+    public void Boolean()
+    {
+        bool mybool = true;
+         Console.WriteLine(mybool);
+        
+    }
 }
