7ef34b7c2aa805a20a14c845cee65db617c2f8f6
ManeeshChintalapudi12 <chintalapudimaneesh@gmail.com>
2025-06-03 17:16:28 -0500

    Displayed the Integer variable

--- Program.cs
+++ Program.cs
@@ -3,5 +3,7 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Variables variables = new Variables();
+        variables.Integer();
     }
 }
---
+++ Variables/Variables.cs
@@ -0,0 +1,8 @@
+public class Variables
+{
+    public void Integer()
+    {
+        int i = 10;
+        Console.WriteLine(i);
+    }
+}
